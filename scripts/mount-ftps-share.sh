99abbd744f40ef1c600e21d83b2d8eb00c76462a
kraloveckey <kraloveckey@groot-mbp>
2024-12-07 14:20:30 +0200

    Upgrade mount-ftps-share.sh script: rclone configuration.

--- scripts/mount-ftps-share.sh
+++ scripts/mount-ftps-share.sh
@@ -5,23 +5,49 @@
 #  $cfg_type = "local";
 #  $cfg_local_path = "/usr/local/fsbackup/archive";
 #
-# Need curlftpfs installed. Require:
-#   * apt install curlftpfs
+# Need rclone installed. Require:
+#   * curl https://rclone.org/install.sh | bash
+#   * apt install fuse3
 #
 
 # FTPS host settings
+FTPS_REMOTE_NAME="lsftp" # Name of the rclone remote
 FTPS_HOST=ftps.dns.com
-FTPS_SHARE=BACKUP
-FTPS_USER=backup
+FTPS_SHARE=/BACKUP
+FTPS_USER=backupusr
 FTPS_PASS=somepassword
 FTPS_CHECK_PORT=21
 
+RCLONE_CONFIG_FILE="$HOME/.config/rclone/rclone.conf"  # Path to rclone config file
+
 LOCAL_MNT_DIR=/usr/local/fsbackup/archive
+
+# Function to check if rclone remote exists
+function remote_exists() {
+    rclone listremotes | grep -q "^${FTPS_REMOTE_NAME}:$"
+}
+
+# Function to create rclone remote
+function create_remote() {
+    echo "Creating rclone remote '$FTPS_REMOTE_NAME'..."
+    rclone config create "$FTPS_REMOTE_NAME" ftp \
+        host "$FTPS_HOST" \
+        user "$FTPS_USER" \
+        pass "$FTPS_PASS" \
+        --obscure \
+        > /dev/null
+
+    # Add additional variables to the remote configuration
+    echo "Adding additional variables to the remote configuration..."
+    sed -i "/^\[${FTPS_REMOTE_NAME}\]/a explicit_tls = true" "$RCLONE_CONFIG_FILE"
+    sed -i "/^\[${FTPS_REMOTE_NAME}\]/a disable_mlsd = false" "$RCLONE_CONFIG_FILE"
+    sed -i "/^\[${FTPS_REMOTE_NAME}\]/a disable_tls13 = true" "$RCLONE_CONFIG_FILE"
+}
 
 # ------------------------------------------------
 if [ _"$1" = _"umount" ]; then
         echo "Ok. Backup done. Unmount share and exit..."
-        if ! umount $LOCAL_MNT_DIR ; then
+        if ! umount -l $LOCAL_MNT_DIR ; then
                 echo "Fail umount share '$LOCAL_MNT_DIR'."
                 exit 1
         fi
@@ -47,23 +73,30 @@ do
         sleep 300
 done
 
+# Main script logic
+if remote_exists; then
+        echo "rclone profile '$FTPS_REMOTE_NAME' already exists."
+else
+        create_remote
+fi
+
 # Check if share already mounted
-if [ -n "`/bin/df -h | egrep \"$FTPS_HOST:$FTPS_SHARE\"`" ]
+if [ -n "`/bin/df -h | egrep \"$FTPS_REMOTE_NAME:\"`" ]
 then
-	echo "Error!  '$FTPS_HOST:$FTPS_SHARE' already mounted (check mounts). Exit."
-	exit 2
+        echo "Error! '$FTPS_HOST:$FTPS_SHARE' already mounted (check mounts). Exit."
+        exit 2
 fi
 
 echo "Ok, try to mount..."
 
-if curlftpfs -o allow_other,ssl $FTPS_USER:$FTPS_PASS@$FTPS_HOST:$FTPS_SHARE $LOCAL_MNT_DIR
+if rclone mount $FTPS_REMOTE_NAME:$FTPS_SHARE $LOCAL_MNT_DIR --vfs-cache-mode writes --daemon
 then
-	echo "Share mount success. Now beginning backup."
+        echo "Share mount success. Now beginning backup :)"
 
-	sleep 1
-	exit 0
+        sleep 1
+        exit 0
 else
 
-	echo "Fail mount $FTPS_HOST:$FTPS_SHARE share. Exit."
-	exit 1
+        echo "Fail mount $FTPS_HOST:$FTPS_SHARE share. Exit :("
+        exit 1
 fi
